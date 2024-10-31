f8a724c5e2c7d8bba1d1f1af9acfc7acd41c1871
Harjobanpreet Sidhu <harjobanpreetsingh.sidhu@student.ufv.ca>
2024-10-30 19:48:32 -0700

    home page added

--- movies_search_engine/app/page.js
+++ movies_search_engine/app/page.js
@@ -2,8 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello Movies Search Engine</h1>
+    <div className="h-376 flex justify-center items-center flex-col gap-4">
+      <h1 className="text-4xl font-bold">Hello Movies Search Engine</h1>
+      <p className="text-2xl">COMP-455 Project</p>
     </div>
   );
 }
--- movies_search_engine/components/movieCard.js
+++ movies_search_engine/components/movieCard.js
@@ -7,7 +7,7 @@ export default function MovieCard({movieData}) {
     <div className='border py-2 px-4 min-w-60 w-1/4'>
         <Image width={250} src={netflix} alt='Netflix Logo'/>
         <div className='my-2'>
-            <h2 className='justify-center text-lg font-semibold h-16 flex items-center'>{movieData.title}</h2>
+            <h2 className='justify-center text-center text-lg font-semibold h-16 flex items-center'>{movieData.title}</h2>
             <p>Type         :   {movieData.type}</p>
             <p>IMDb Rating  :   {movieData.imdbAverageRating}</p>
             <p>Release Year :   {movieData.releaseYear}</p>
